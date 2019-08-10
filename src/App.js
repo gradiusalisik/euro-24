448d663edad293a356d63225e1a7ada878f553cd
Vladislav Aleev <vlad.aleev@gmail.com>
2019-08-10 09:03:03 +0200

    add fixes to Checkbox component 2

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import Cupboard from "./components/Cupboard/Cupboard";
 import Services from "./components/Services/Services";
 import Nets from "./components/Nets/Nets";
 import Window from "./components/Window/Window";
-import Checkbox from "./components/Checkbox/Checkbox";
 import { AppStyled, HeaderScroll } from "./App.styled.js";
 
 const App = () => {
@@ -45,7 +44,6 @@ const App = () => {
 
       <HeaderScroll isScroll isShowHeader={isShowHeader} />
       <FirstScreen />
-      <Checkbox>twst</Checkbox>
       <Element name="window">
         <Window />
         <Jalousie />
