441a51cbd7bd2557b9f04f799749706f90ed263a
Vladislav Aleev <vlad.aleev@gmail.com>
2019-08-18 12:22:27 +0200

    add socials to json

--- src/components/SocialIcons/SocialIcons.js
+++ src/components/SocialIcons/SocialIcons.js
@@ -1,21 +1,20 @@
 import React from "react";
-import { Facebook } from "../Icons/Facebook";
-import { Instagram } from "../Icons/Instagram";
-import { Twitter } from "../Icons/Twitter";
 
 import { SocialIconsStyled, SocialIconsLink} from "./SocialIcons.styled";
 
+import { socialIcons } from "../../assets/dataJson/socialIcons";
+
 const SocialIcons = () => (
   <SocialIconsStyled>
-    <SocialIconsLink href="#" rel="noopener nofollow">
-      <Facebook />
-    </SocialIconsLink>
-    <SocialIconsLink href="#" rel="noopener nofollow">
-      <Twitter />
-    </SocialIconsLink>
-    <SocialIconsLink href="#" rel="noopener nofollow">
-      <Instagram />
-    </SocialIconsLink>
+    {socialIcons.map(icon => (
+      <SocialIconsLink
+        key={icon.id}
+        href={icon.href}
+        rel="noopener nofollow"
+        >
+        {icon.test}
+      </SocialIconsLink>
+    ))}
   </SocialIconsStyled>
 );
 
